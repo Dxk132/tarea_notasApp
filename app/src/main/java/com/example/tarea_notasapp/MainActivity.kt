649b64903448b67e6a721e32f9163157aa5fe796
Dxk132 <sebas.casalfa@gmail.com>
2026-09-10 22:05:02 -0600

    Mostrar notas en la BD

--- app/src/main/java/com/example/tarea_notasapp/MainActivity.kt
+++ app/src/main/java/com/example/tarea_notasapp/MainActivity.kt
@@ -6,16 +6,25 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.tarea_notasapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
+    private lateinit var db : NotaDatabaseHelper
+    private lateinit var notasAdaptador: NotasAdaptador
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        db = NotaDatabaseHelper(this)
+        notasAdaptador= NotasAdaptador(db.getAllNotas(),this)
+        binding.notasRv.layoutManager = LinearLayoutManager(this)
+        binding.notasRv.adapter = notasAdaptador
+
+
         binding.FABAgregarNota.setOnClickListener {
             val intent = Intent(this, AgregarNotaActivity::class.java)
             startActivity(intent)
@@ -26,5 +35,10 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        notasAdaptador.refrescarlista(db.getAllNotas())
     }
 }
