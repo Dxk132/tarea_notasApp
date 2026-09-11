b83f94c1bd2614119cc3850c5395ae7c2acd9689
Dxk132 <sebas.casalfa@gmail.com>
2026-09-11 17:57:07 -0600

    Consulta para obtener la informacion de la nota seleccionada

--- app/src/main/java/com/example/tarea_notasapp/NotaDatabaseHelper.kt
+++ app/src/main/java/com/example/tarea_notasapp/NotaDatabaseHelper.kt
@@ -54,6 +54,21 @@ class NotaDatabaseHelper(context: Context) : SQLiteOpenHelper(
         return listaNotas
     }
 
+    fun getNotaById(idNota: Int): Nota {
+        val db = readableDatabase
+        val query = "SELECT * FROM $TABLE_NAME WHERE $COLUMN_ID = $idNota"
+        val cursor = db.rawQuery(query, null)
+        cursor.moveToFirst()
+
+        val id = cursor.getInt(cursor.getColumnIndexOrThrow(COLUMN_ID))
+        val titulo = cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_TITLE))
+        val descripcion = cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_DESCRIPTION))
+
+        cursor.close()
+        db.close()
+        return Nota(id,titulo,descripcion)
+    }
+
     companion object {
         private const val DATABASE_NAME = "notas.db"
         private const val DATABASE_VERSION = 1
